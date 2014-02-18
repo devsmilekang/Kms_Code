6a3747c77e395450a272d40f01c2a781459fea08
kangminser <kangminser88@gmail.com>
2014-02-18 17:23:05 +0900

    commaAdd,commaRemove 추가

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,17 @@
 package Main;
 
+import kmsUtill.KmsMethod;
+
 public class Main {
 	public static void main(String args[]){
-		System.out.println("aa");
+		KmsMethod kms = new KmsMethod();
+	
+		//¼ıÀÚÄŞ¸¶Âï±â
+		//System.out.print("¼ıÀÚ ÄŞ¸¶Âï±â : ");
+		//System.out.println(kms.commaAdd(2000000000));
+		
+		//¼ıÀÚÄŞ¸¶Á¦°Å
+		//System.out.print("¼ıÀÚ ÄŞ¸¶Á¦°Å : ");
+		//System.out.println(kms.commaRemove("2,000,000"));
 	}
 }
--- src/kmsUtill/KmsMethod.java
+++ src/kmsUtill/KmsMethod.java
@@ -1,5 +1,28 @@
 package kmsUtill;
 
-public class KmsMethod {
+//모든 메소드는 여기에 담아 놓을꺼야
 
+public class KmsMethod {
+	public KmsMethod() {}
+	//숫자 콤마 찍기
+	public String commaAdd(Object num){
+		String original = String.valueOf(num);
+		String convert = "";
+		int count = 1;
+		for(int k=original.length()-1; k>-1; k--){			
+			if( (count%3) == 0 && k < original.length()-1 && k > 0){
+				convert = "," +original.charAt(k)  + convert;
+			}
+			else{
+				convert = original.charAt(k) + convert;
+			}
+			count++;
+		}
+		return convert;
+	}
+	
+	// 숫자 콤마 제거
+	public String commaRemove(Object num){
+		return String.valueOf(num).replaceAll(",", "");
+	}
 }
