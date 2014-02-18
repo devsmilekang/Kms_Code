491495aac80d50b9f4f5bb6fad592cc870b3ea8d
kangminser <kangminser88@gmail.com>
2014-02-18 17:33:35 +0900

    isNull 함수 추가

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -13,5 +13,9 @@ public class Main {
 		//숫자콤마제거
 		//System.out.print("숫자 콤마제거 : ");
 		//System.out.println(kms.commaRemove("2,000,000"));
+		
+		//Null, 빈값일 때 문자변환
+		//System.out.print("Null, 빈값일 때 문자변환 ");
+		//System.out.println(kms.isNull("", "null 값"));
 	}
 }
--- src/kmsUtill/KmsMethod.java
+++ src/kmsUtill/KmsMethod.java
@@ -5,8 +5,8 @@ package kmsUtill;
 public class KmsMethod {
 	public KmsMethod() {}
 	//숫자 콤마 찍기
-	public String commaAdd(Object num){
-		String original = String.valueOf(num);
+	public static String commaAdd(Object num){
+		String original = String.valueOf(num).trim();
 		String convert = "";
 		int count = 1;
 		for(int k=original.length()-1; k>-1; k--){			
@@ -25,4 +25,18 @@ public class KmsMethod {
 	public String commaRemove(Object num){
 		return String.valueOf(num).replaceAll(",", "");
 	}
+	
+	//Null값을 빈 값으로 돌려주기
+	public String isNull(Object obj){
+		return isNull(obj, "");
+	}
+	public String isNull(Object obj, String str){
+		if(obj == null || "".equals(String.valueOf(obj).trim()) || "null".equals(String.valueOf(obj).trim())){
+			return str;
+		}
+		else{
+			return String.valueOf(obj);
+		} 
+	}
+	
 }
